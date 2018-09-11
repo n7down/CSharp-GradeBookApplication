23684dd8228fc7190c06098319893ad15d7c056f
Joshua Adams <n7down@gmail.com>
2018-09-11 00:11:06 -0600

    working on problem

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -18,7 +20,41 @@ namespace GradeBook.GradeBooks
             }
 			var totalNumberOfStudents = Students.Count;
 			var numberOfStudentsToDropLetterGrade = totalNumberOfStudents * .2;
-            return 'F';
+		
+			// list students by average grade in asending order
+			List<double> averageGradeList = Students.OrderBy(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();	
+			var numberOfStudents = 0;
+			var letterGrade = 'A';
+			var averageGradeLetterGradeDictionary = new Dictionary<double, char>();
+			foreach(double grade in averageGradeList)
+			{
+				averageGradeLetterGradeDictionary.Add(grade, letterGrade);
+				numberOfStudents++;
+				if(numberOfStudents >= numberOfStudentsToDropLetterGrade)
+				{
+					numberOfStudents = 0;
+					switch(letterGrade)
+					{
+						case 'A':
+							letterGrade = 'B';
+							break;
+						case 'B':
+							letterGrade = 'C';
+							break;
+						case 'C':
+							letterGrade = 'D';
+							break;
+						case 'D':
+							letterGrade = 'F';
+							break;
+						case 'F':
+							break;
+						default:
+							break;
+					}
+				}
+			}
+			return averageGradeLetterGradeDictionary[averageGrade];
         }
     }
 }
