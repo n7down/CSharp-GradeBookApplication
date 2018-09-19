9817d4e6af589481e96a2fbdc1719f864abc9e20
Joshua Adams <n7down@gmail.com>
2018-09-19 16:38:55 -0600

    updated with override methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,7 +22,7 @@ namespace GradeBook.GradeBooks
 			var numberOfStudentsToDropLetterGrade = totalNumberOfStudents * .2;
 		
 			// list students by average grade in asending order
-			List<double> averageGradeList = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();	
+			var averageGradeList = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();	
 			var numberOfStudents = 0;
 			var letterGrade = 'A';
 			var averageGradeLetterGradeDictionary = new Dictionary<double, char>();
@@ -56,5 +56,25 @@ namespace GradeBook.GradeBooks
 			}
 			return averageGradeLetterGradeDictionary[averageGrade];
         }
+
+		public override void CalculateStatistics()
+		{
+			if(Students.Count() < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+				return;
+			}
+			base.CalculateStatistics();
+		}
+
+		public override void CalculateStudentStatistics(string name)
+		{
+			if(Students.Count() < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+				return;
+			}
+			base.CalculateStudentStatistics(name);
+		}
     }
 }
